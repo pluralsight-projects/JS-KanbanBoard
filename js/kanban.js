128694586c1291c3a0a661acf28fc9bdef5d5b8a
douglas parnoff <douglasdasilva.parnoff@adp.com>
2019-11-20 12:50:25 -0200

    set dragend event listener of item

--- js/kanban.js
+++ js/kanban.js
@@ -22,6 +22,7 @@ const create_item = () => {
   item.draggable = true;
 
   item.addEventListener('dragstart', event => event.dataTransfer.setData('text', event.target.id));
+  item.addEventListener('dragend', event => event.dataTransfer.clearData());
 };
 
 document.querySelectorAll('.drop').forEach(element => {
